7701de6d0d2c7481de7c114aa37565dd420beea0
yusuf0786 <yusssuf0786@gmail.com>
2025-06-15 22:34:15 +0530

    fixes for the build error

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,15 +1,20 @@
-import { getProjectById } from "@/lib/projects";
+import { getProjectById, projects } from "@/lib/projects";
 import ProjectModal from "./ProjectModal";
 
-export default async function Page({ params }: {params: { id: string }}) {
+export default async function Page({ params }: { params: { id: string } }) {
   const project = await getProjectById(params.id);
   if (!project) {
     return <div>Project not found.</div>;
   }
- return (
+  return (
     <div>
       <ProjectModal project={project} />
     </div>
- )
-  
+  );
+}
+
+export async function generateStaticParams() {
+  return projects.map((project) => ({
+    params: { id: project.id },
+  }));
 }
