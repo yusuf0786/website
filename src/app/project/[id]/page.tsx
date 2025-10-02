84a69167f6717fd50617d860479a557cf477b79c
yusuf0786 <yusssuf0786@gmail.com>
2025-10-02 13:01:47 +0530

    project page parmas props type naming convention correctiion

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,19 +1,17 @@
 import { getProjectById } from "@/lib/projects";
 import ProjectModal from "./ProjectModal";
 import { serialize } from "next-mdx-remote/serialize";
+import { notFound } from "next/navigation";
 
 type ProjectPageParamsType = {
-  params: Promise<{ id: string }>
+  params: Promise<{ id: string }>;
 };
 
 export default async function ProjectPage({ params }: ProjectPageParamsType) {
-  const { id: rawId } = await params;
-
-  const id = typeof rawId === "string" ? rawId : Array.isArray(rawId) ? rawId[0] : undefined;
-  if (!id) return <div>Project not found</div>;
+  const { id } = await params;
 
   const project = getProjectById(id);
-  if (!project) return <div>Project not found</div>;
+  if (!project) return notFound();
 
   const mdxSource = await serialize(project.description);
   return <ProjectModal project={{ ...project, descriptionMdx: mdxSource }} />;
